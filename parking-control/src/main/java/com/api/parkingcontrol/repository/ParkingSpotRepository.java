c1d273d86fedf7e77182e710a62e6df31c5960a1
Edson de Almeida <88493489+edsondealmeida@users.noreply.github.com>
2023-03-03 10:51:56 +0200

    Uploading parking-spot project to github

---
+++ parking-control/src/main/java/com/api/parkingcontrol/repository/ParkingSpotRepository.java
@@ -0,0 +1,20 @@
+package com.api.parkingcontrol.repository;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository <ParkingSpotModel, UUID> {
+
+	boolean existsByLicencePlateCar(String licencePlateCar);
+	
+	boolean existsByParkingSpotNumber(String parkingSpotNumber);
+	
+	boolean existsByApartmentAndBlock(String apartment, String block);
+	
+}
